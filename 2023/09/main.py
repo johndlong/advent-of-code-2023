4b00094c02bdcd32f1ede1f40fd8017540e5a838
John Long <john.d.long@gmail.com>
2023-12-11 10:45:30 -0500

    Day 9 - Part 1

---
+++ 2023/09/main.py
@@ -0,0 +1,59 @@
+"""Day 9: Mirage Maintenance """
+from __future__ import annotations
+import argparse
+import re
+
+
+def read_file(path: str) -> list[list[int]]:
+    """Returns a list of list of values that are the basis for predictions."""
+    with open(path, encoding="utf-8") as f:
+        data = f.read()
+
+    lines = data.splitlines()
+
+    sequence = []
+    sequence_regex = re.compile(r"([-]?\d+)")
+
+    for line in lines:
+        values = []
+        for sequence_match in sequence_regex.finditer(line):
+            values.append(int(sequence_match.group(0)))
+        sequence.append(values)
+
+    return sequence
+
+
+def prediction(values: list[int]) -> int:
+    diffs = []
+    if len(values) == 0:
+        return 0
+    i = values[0]
+    for j in values[1:]:
+        diffs.append(j - i)
+        i = j
+
+    if set(diffs) == set([0]):
+        return values[-1:][0]
+    else:
+        return values[-1:][0] + prediction(diffs)
+
+
+def part1(sequences: list[list[int]]) -> int:
+    total = 0
+    for seq in sequences:
+        total += prediction(seq)
+    return total
+
+
+def main():
+    """Main entrypoint."""
+    parser = argparse.ArgumentParser(prog="day9")
+    parser.add_argument("-f", "--filename", required=True)
+    args = parser.parse_args()
+    sequences = read_file(args.filename)
+    p1 = part1(sequences)
+    print(f"Part 1: {p1}")
+
+
+if __name__ == "__main__":
+    main()
