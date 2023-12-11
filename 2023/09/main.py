cbc8b8ab7ddcab34e59f10598939cbf446f95369
John Long <john.d.long@gmail.com>
2023-12-11 10:57:52 -0500

    Day 9 - Part 2

--- 2023/09/main.py
+++ 2023/09/main.py
@@ -23,7 +23,7 @@ def read_file(path: str) -> list[list[int]]:
     return sequence
 
 
-def prediction(values: list[int]) -> int:
+def part1_prediction(values: list[int]) -> int:
     """Returns the prediction value given the set of values."""
     diffs = []
     if len(values) == 0:
@@ -36,14 +36,38 @@ def prediction(values: list[int]) -> int:
     if set(diffs) == set([0]):
         return values[-1:][0]
 
-    return values[-1:][0] + prediction(diffs)
+    return values[-1:][0] + part1_prediction(diffs)
+
+
+def part2_prediction(values: list[int]) -> int:
+    """Returns the left-side prediction value given the set of values."""
+    diffs = []
+    if len(values) == 0:
+        return 0
+    i = values[0]
+    for j in values[1:]:
+        diffs.append(j - i)
+        i = j
+
+    if set(diffs) == set([0]):
+        return values[0]
+
+    return values[0] - part2_prediction(diffs)
 
 
 def part1(sequences: list[list[int]]) -> int:
     """Returns the sum of predictions given a sequence of values."""
     total = 0
     for seq in sequences:
-        total += prediction(seq)
+        total += part1_prediction(seq)
+    return total
+
+
+def part2(sequences: list[list[int]]) -> int:
+    """Returns the sum of predictions given a sequence of values."""
+    total = 0
+    for seq in sequences:
+        total += part2_prediction(seq)
     return total
 
 
@@ -55,6 +79,9 @@ def main():
     sequences = read_file(args.filename)
     p1 = part1(sequences)
     print(f"Part 1: {p1}")
+
+    p2 = part2(sequences)
+    print(f"Part 2: {p2}")
 
 
 if __name__ == "__main__":
