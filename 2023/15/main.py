a18add851f2361940da0ea1350a11f17e3435f6e
John Long <2533425+johndlong@users.noreply.github.com>
2023-12-19 15:43:31 -0500

    Day 15 - Part 2 (#28)

--- 2023/15/main.py
+++ 2023/15/main.py
@@ -2,15 +2,57 @@
 
 from __future__ import annotations
 from argparse import ArgumentParser
+from collections import defaultdict, OrderedDict
+from dataclasses import dataclass
+from enum import Enum
+import re
+from typing import Optional
 
 
-def read_file(path: str) -> list[str]:
+class Operation(Enum):
+    """Defines the types of operations"""
+
+    REMOVE = "-"
+    ADD = "="
+
+
+@dataclass
+class Label:
+    """Defines a label"""
+
+    name: str
+    number: Optional[int] = 0
+
+
+@dataclass
+class LabelEntry:
+    """Defines a label entry"""
+
+    label: Label
+    operation: Operation
+
+
+def read_file(path: str, label: bool = False) -> list[str] | list[Operation]:
     """Returns a data set given the file provided."""
     with open(path, encoding="utf-8") as f:
         data = f.read()
 
     data = data.replace("\n", "")
-    return data.split(",")
+
+    if not label:
+        return data.split(",")
+
+    retval = []
+    data_re = re.compile(r"(\w+)([-=])(\d+)?")
+    for d in data.split(","):
+        result = data_re.match(d)
+        if not result:
+            raise ValueError(f"failed parsing data: {d}")
+        label = Label(name=result.group(1))
+        if result.group(3):
+            label.number = int(result.group(3))
+        retval.append(LabelEntry(label=label, operation=Operation(result.group(2))))
+    return retval
 
 
 def calculate_hash(s: str) -> int:
@@ -28,6 +70,28 @@ def part1(data: list[str]) -> int:
     return sum(results)
 
 
+def part2(data: list[LabelEntry]) -> int:
+    """Returns the part 2 answer"""
+    buckets = defaultdict(OrderedDict)
+    for d in data:
+        hash_val = calculate_hash(d.label.name)
+        bucket = buckets[hash_val]
+        if d.operation == Operation.REMOVE:
+            if d.label.name in bucket:
+                del bucket[d.label.name]
+        else:
+            bucket[d.label.name] = d.label
+
+    retval = 0
+    for i in range(0, 256):
+        if i not in buckets or len(buckets[i]) == 0:
+            continue
+        bucket = buckets[i]
+        for j, slot in enumerate(list(bucket.keys()), 1):
+            retval += (i + 1) * (j * bucket[slot].number)
+    return retval
+
+
 def main():
     """Main entrypoint."""
     parser = ArgumentParser(prog="day15")
@@ -36,6 +100,10 @@ def main():
     data = read_file(args.filename)
     p1 = part1(data)
     print(f"Part1: {p1}")
+
+    d2 = read_file(args.filename, label=True)
+    p2 = part2(d2)
+    print(f"Part2: {p2}")
 
 
 if __name__ == "__main__":
